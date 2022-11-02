3c22463af82fc08e08538cdda1f1527767562808
yhfwork <mm20102010mm>
2022-11-03 05:01:04 +0800

    Update f_yc_pangrowth_initializer.dart

--- lib/f_yc_pangrowth_initializer.dart
+++ lib/f_yc_pangrowth_initializer.dart
@@ -1,18 +1,18 @@
-import 'dart:developer';
-
 import 'package:f_yc_config/f_yc_config.dart';
 import 'package:f_yc_pangrowth/src/novel/f_yc_pangrowth_novel.dart';
 import 'package:f_yc_pangrowth/src/video/f_yc_pangrowth_video.dart';
 
 /// 初始化
 class FYcPangrowthInitializer {
+  static FYcPangrowthNovel pangrowthNovel = FYcPangrowthNovel();
+  static FYcPangrowthVideo pangrowthVideo = FYcPangrowthVideo();
+
   static initializer({int initializerType = 0, bool debug = false}) async {
     FYcConfigPangrowthConfig pangrowthConfig = FYcConfigConfigurator.instance
         .getConfig(configId: KIT_CONFIG_ID)
         .pangrowthConfig;
-    log('---FYcPangrowthInitializer----${pangrowthConfig.commonConfig.appNameEn}');
     if (initializerType == 1 || initializerType == 0) {
-      await FYcPangrowthNovel().registerNovel(
+      await pangrowthNovel.registerNovel(
           appName: pangrowthConfig.commonConfig.appNameEn,
           andoridAppId: pangrowthConfig.andoridAppId,
           iosAppId: pangrowthConfig.iosAppId,
@@ -23,7 +23,7 @@ class FYcPangrowthInitializer {
           debug: debug);
     }
     if (initializerType == 2 || initializerType == 0) {
-      await FYcPangrowthVideo().registerVideo(
+      await pangrowthVideo.registerVideo(
           appName: pangrowthConfig.commonConfig.appNameEn,
           andoridAppId: pangrowthConfig.andoridAppId,
           iosAppId: pangrowthConfig.iosAppId,
